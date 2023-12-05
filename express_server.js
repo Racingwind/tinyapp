633e16161f9b09aa43b608cde13ce81a5ad9c35d
Racingwind <louis.k.hu@gmail.com>
2023-12-04 18:19:36 -0800

    added route to handle /login

--- express_server.js
+++ express_server.js
@@ -73,6 +73,11 @@ app.post("/urls/:id/update", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("Username", req.body.username);
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
